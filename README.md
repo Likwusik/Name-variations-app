096beae6fa39fa584b263ef1cf095738baa04373
Likwusik <angelimakaro@gmail.com>
2024-05-13 15:50:37 +0200

    Some changes

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 # Name Variations App
 
-Welcome to the Name Variations App! This mini-project was designed to help me practice my JavaScript skills by creating a simple application that generates variations of a given name.
+Welcome to the Name Variations App! This mini-project was created to help me practice my JavaScript skills by creating a simple application that generates variations of a given name.
 
 ## Overview
 
-The Name Variations App takes a user-inputted name and generates different variations based on predefined rules. These variations include lowercase, uppercase, alternating case, reversed, and more. It also counts 
+The Name Variations App takes a user-inputted name and generates different variations based on predefined rules. These variations include lowercase, uppercase, alternating case, reversed, and more. It also counts a number of characters in the given name. 
+
+## Usage
+
+To use the Name Variations App:
+
+1. Enter a name into the input field.
+2. View the generated variations below the input field.
 
 
+## Installation
+
+To run this project locally:
+
+1. Clone this repository to your local machine:
+2. Open the `index.html` file in your web browser.
 
--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-import {getNumberOfChars, getLower, getUpper, getReverse, getAltCase} from "./function.js"
 
 let number = document.querySelector("#number")
 let lower = document.querySelector("#lower")
 let upper = document.querySelector("#upper")
 let altCase = document.querySelector("#altCase")
 let reverse = document.querySelector("#reverse")
+let addHeart = document.querySelector("#addHeart")
+
 
 let name = document.querySelector("#your-name")
 
@@ -14,4 +15,51 @@ name.addEventListener("keyup", () => {
     upper.textContent = getUpper(name.value)
     reverse.textContent = getReverse(name.value)
     altCase.textContent = getAltCase(name.value)
+    addHeart.textContent = getaddHeart(name.value)
 });
+
+function getNumberOfChars(name) {
+    // return the number of characters
+return name.length;
+}
+
+function getLower(name) {
+    // return name all in lower case 
+return name.toLowerCase();
+}
+
+function getUpper(name) {
+    // return name all in upper case 
+return name.toUpperCase();
+}
+
+function getReverse(name) {
+    // return reversed name 
+const upperCaseName = name.toUpperCase();
+    // Split the name into an array of characters, reverse it, then join it back into a string
+    const reversedName = upperCaseName.split('').reverse().join('');
+
+    return reversedName.toLowerCase(-1);
+}
+
+function getAltCase(name) {
+    var resultAlt = '';
+    for (var i = 0; i < name.length; i++) {
+        if (i % 2 === 0) {
+            resultAlt += name[i].toUpperCase();
+        } else {
+            resultAlt += name[i].toLowerCase();
+        }
+    }
+    return resultAlt;
+}
+
+function getaddHeart(name) {
+    let nameWithHearts = '';
+    for (let i = 0; i < name.length; i++) {
+        nameWithHearts += name[i] + '❤️';
+    }
+    // Remove the extra heart at the end
+    nameWithHearts = nameWithHearts.slice(0, -2);
+    return nameWithHearts;
+}
